9f536cbe72a1295150abacb8e333d6c0a967e9ed
Tak <takeshiseodev@gmail.com>
2023-12-26 18:09:44 -0500

    update

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 /**
- * 🎄 Challenge: 
- * 1. The Christmas tree's lights should switch 
+ * 🎄 Challenge:
+ * 1. The Christmas tree's lights should switch
  *    on and off every 800 miliseconds.
- * 
+ *
  * Stretch Goal:
  *    Make the blue and red lights flash alternately.
- **/ 
+ *
+ * @format
+ */
+
+
+setInterval(() => {
+    document.querySelectorAll('.lights').forEach(light => {
+        light.classList.toggle('lights-on')
+    })
+}, 800)
+
+
+let toggle = true
+
+setInterval(() => {
+    const redLights = document.getElementsByClassName('red')
+    const blueLights = document.getElementsByClassName('blue')
+
+    if (toggle) {
+        for (let light of redLights) light.classList.add('lights-on')
+        for (let light of blueLights) light.classList.remove('lights-on')
+    } else {
+        for (let light of redLights) light.classList.remove('lights-on')
+        for (let light of blueLights) light.classList.add('lights-on')
+    }
+
+    toggle = !toggle
+}, 800)
